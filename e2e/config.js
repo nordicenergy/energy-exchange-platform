36894f40f6bbca2d988301c095904f576435b593
Nordic Energy Dev <54575925+nordicenergy@users.noreply.github.com>
2020-03-10 16:56:50 +0200

    Update config.js

--- e2e/config.js
+++ e2e/config.js
@@ -1,5 +1,5 @@
 const config = {
-    host: 'https://localhost:3000',
+    host: 'https://exchange.nordicenergy.co',
     credentials: {
         username: 'powerchainuser',
         password: 'powerchainpass1'
